e93d5f06317414f95d70ff92e61193ed2e46c20b
Ji Lee <gafrong@gmail.com>
2014-11-27 08:55:17 +1100

    todo backbone

---
+++ gumballs/ji_lee/wk9d3/todor/app/assets/javascripts/views/todo_item_view.js
@@ -0,0 +1,36 @@
+var App = App || {};
+
+App.Views.TodoItemView = Backbone.View.extend ({
+
+  tagName: 'li',
+  events: {
+    'click': 'selectTodo',
+    'click .del': 'deleteTodo'
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+  },
+
+  render: function() {
+    var template = $('#todoItemView').html();
+    var todoItemHTML = Handlebars.compile(template);
+
+    this.$el.html(todoItemHTML( this.model.toJSON() ));
+    return this;
+  },
+
+  selectTodo: function() {
+    this.$el.parent().find('li').removeClass('highlight');
+    this.$el.addClass('highlight');
+    $('#todo').find('p').hide().html(this.model.get("body")).fadeIn();
+  },
+
+  deleteTodo: function(event) {
+    event.preventDefault();
+    this.model.destroy();
+    this.$el.fadeOut(700, function(){
+      this.remove();      
+    })
+  }
+});
---
+++ gumballs/ji_lee/wk9d3/todor/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  root 'todos#index'
+  resources :todos
+  
+end
